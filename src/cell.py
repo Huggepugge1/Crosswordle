318248e56f2ffe9ee8a3137f49494bacc2e44d43
William Olsson <williamolsson2004@gmail.com>
2023-10-17 14:34:56 +0200

    updated cell.py

--- src/cell.py
+++ src/cell.py
@@ -1,4 +1,11 @@
 class Cell:
-    def __init__():
-        pass
+    def __init__(self, position: tuple[int, int], state: bool, words: tuple[str, str], current_content: str, solution: str):
+        self.position = position
+        self.state = state
+        self.words = words
+        self.current_content = current_content
+        self.solution = solution
+
+def make_cells_from_user_state(user_state):
+    pass
 
